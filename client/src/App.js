b29e85f1e4539a0f146d8dbf65c8bb5060e0b2cb
Mari <43574832+marisoto@users.noreply.github.com>
2025-05-10 04:37:27 -0400

    test 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// client/src/App.js
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PostList from './components/PostList';
+import PostDetail from './components/PostDetail';
+import NewPost from './components/NewPost';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PostList />} />
+        <Route path="/post/:id" element={<PostDetail />} />
+        <Route path="/new" element={<NewPost />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
